c4e62374dbf5935d3094c827f792bf919deb97a7
desonglll <lindesong666@163.com>
2025-01-28 20:48:39 +0800

    Update

---
+++ lua/plugins.lua
@@ -0,0 +1,25 @@
+vim.cmd([[
+call plug#begin()
+
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+
+Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+
+Plug 'L3MON4D3/LuaSnip'
+Plug 'saadparwaiz1/cmp_luasnip'
+
+
+Plug 'williamboman/mason.nvim'
+Plug 'williamboman/mason-lspconfig.nvim'
+
+call plug#end()
+]])
+
+require("plugins.nvim-cmp")
+require("plugins.mason")
+require("plugins.mason-lspconfig")
---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,11 @@
+require("mason-lspconfig").setup({
+    automatic_installation = true, -- 启用自动安装
+})
+
+-- 获取所有可用的 LSP 服务器名称
+local lsp_servers = require("mason-lspconfig").get_installed_servers()
+
+-- 为每个已安装的 LSP 配置基础设置
+for _, server_name in ipairs(lsp_servers) do
+    require("lspconfig")[server_name].setup({})
+end
